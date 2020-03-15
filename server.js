6f3cf11a69f41bb1049b62161862d5300df8f07f
kieuchicong99 <kieuchicong99@gmail.com>
2020-03-15 22:23:21 +0700

    done

--- server.js
+++ server.js
@@ -1,38 +1,9 @@
 const WebSocket = require("ws");
+const { listStudent } = require("./students");
 const wss = new WebSocket.Server(
   { port: 8080 },
   console.log("WebSocket Server is listening on port 8080!")
 );
-
-const listStudent = [
-  {
-    mssv: "17020617",
-    name: "Kiều Chí Công",
-    dateOfBirth: "13/06/1999",
-    phone: "0795038669",
-    address: "Sơn Tây, Hà Nội",
-    email: "kieuchicong99@gmail.com",
-    class: "QH-2017-CQ-CLC"
-  },
-  {
-    mssv: "17021069",
-    name: "Bùi Đặng Thu Trà",
-    dateOfBirth: "02/10/1999",
-    phone: "09998888",
-    address: "Chương Mỹ, Hà Nội",
-    email: "buidangthutra@gmail.com",
-    class: "QH-2017-CQ-K"
-  },
-  {
-    mssv: "17020715",
-    name: "Nguyễn Đức Hải",
-    dateOfBirth: "03/11/1999",
-    phone: "0123456789",
-    address: "Thái Bình",
-    email: "nguyenduchai@gmail.com",
-    class: "QH-2017-CQ-K"
-  }
-];
 
 wss.on("connection", ws => {
   let result;
---
+++ students.js
@@ -0,0 +1,30 @@
+const listStudent = [
+  {
+    mssv: "17020617",
+    name: "Kiều Chí Công",
+    dateOfBirth: "13/06/1999",
+    phone: "0795038669",
+    address: "Sơn Tây, Hà Nội",
+    email: "kieuchicong99@gmail.com",
+    class: "QH-2017-CQ-CLC"
+  },
+  {
+    mssv: "17021069",
+    name: "Bùi Đặng Thu Trà",
+    dateOfBirth: "02/10/1999",
+    phone: "09998888",
+    address: "Chương Mỹ, Hà Nội",
+    email: "buidangthutra@gmail.com",
+    class: "QH-2017-CQ-K"
+  },
+  {
+    mssv: "17020715",
+    name: "Nguyễn Đức Hải",
+    dateOfBirth: "03/11/1999",
+    phone: "0123456789",
+    address: "Thái Bình",
+    email: "nguyenduchai@gmail.com",
+    class: "QH-2017-CQ-K"
+  }
+];
+module.exports = { listStudent };
